33a26ec5be736fc9476a93ca556175dbfc2f07f9
Tsarenko Ivan <thegellios@gmail.com>
2020-01-09 14:08:40 +0200

    Add simple anim Image

---
+++ Assets/Scripts/CircleView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI.ProceduralImage;
+
+public class CircleView : MonoBehaviour
+{
+    [SerializeField] private ProceduralImage image;
+
+    [SerializeField] private int time;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (image.fillAmount < 1)
+            image.fillAmount += Time.deltaTime / time;
+        else
+            image.fillAmount = 0;
+    }
+}
