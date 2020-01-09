7925b42b15b47750eac17a7c7e175a145800e824
Tsarenko Ivan <thegellios@gmail.com>
2020-01-09 18:28:45 +0200

    Add simple anim Image

--- Assets/Scripts/CircleView.cs
+++ Assets/Scripts/CircleView.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI.ProceduralImage;
 
@@ -7,14 +7,27 @@ public class CircleView : MonoBehaviour
 {
     [SerializeField] private ProceduralImage image;
 
+    [SerializeField] private TextMeshProUGUI timeTxt;
+
     [SerializeField] private int time;
+
+    private void Start()
+    {
+        timeTxt.text = "0";
+    }
 
     // Update is called once per frame
     private void Update()
     {
         if (image.fillAmount < 1)
+        {
             image.fillAmount += Time.deltaTime / time;
+            timeTxt.text = (image.fillAmount * 10).ToString("F");
+        }
         else
+        {
             image.fillAmount = 0;
+            timeTxt.text = time.ToString("F");
+        }
     }
 }
