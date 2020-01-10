e0356b887aaea8099a2a4bef5b102f7d4cf42c47
Tsarenko Ivan <thegellios@gmail.com>
2020-01-10 16:38:32 +0200

    add dropable and drapable view

---
+++ Assets/Scripts/AbstractViews/DroppableView.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace View.AbstractViews
+{
+    public abstract class DroppableView : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+    {
+        
+        /// <inheritdoc />
+        /// <summary>
+        /// On drop 
+        /// </summary>
+        /// <param name="eventData"></param>
+        public abstract void OnDrop(PointerEventData eventData);
+
+        /// <inheritdoc />
+        /// <summary>
+        /// On pointer enter
+        /// </summary>
+        /// <param name="eventData"></param>
+        public virtual void OnPointerEnter(PointerEventData eventData)
+        {
+            if (eventData.pointerDrag == null)
+                return;
+
+                  
+        }
+
+        /// <inheritdoc />
+        /// <summary>
+        /// On pointer exit
+        /// </summary>
+        /// <param name="eventData"></param>
+        public virtual void OnPointerExit(PointerEventData eventData)
+        {
+            if (eventData.pointerDrag == null)
+                return;
+
+            var draggableCard = eventData.pointerDrag.GetComponent<HandItemView>();
+
+            if (draggableCard == null || draggableCard.PlaceholderParent != transform)
+                return;
+            if (draggableCard.Placeholder != null)
+            {
+                Destroy(draggableCard.Placeholder.gameObject);
+            }
+
+            draggableCard.PlaceholderParent = draggableCard.ParentToReturnTo;
+        }
+    }
+}
