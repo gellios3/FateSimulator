77e689f84de7309bf80d5ea0f1d8b58a625dbaf3
Gellios3 <thegellios@gmail.com>
2020-05-04 21:35:24 +0300

    some refactoring

--- Assets/Scripts/Canvas/Activities/Installers/ActivitiesInstaller.cs
+++ Assets/Scripts/Canvas/Activities/Installers/ActivitiesInstaller.cs
@@ -7,15 +7,10 @@ namespace Canvas.Activities.Installers
 {
     public class ActivitiesInstaller : MonoInstaller
     {
-        // [Inject] private List<BaseCardObj> CardList { get; }
 
-        [SerializeField] private GameObject activitiesController;
-        
         public override void InstallBindings()
         {
-            // Debug.LogError($"CardList {CardList.Count}");
             Container.Bind<ActivityService>().AsSingle();
-            // Container.Bind<ActivitiesController>().FromComponentOn(activitiesController).AsSingle();
         }
 
     }
